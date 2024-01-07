392e4df24fd618796d05e75bbeda4a537eba0b53
Thada Wangthammang <thada.wth@gmail.com>
2024-01-08 02:15:15 +0700

    add http trigger for test

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import 'dotenv/config';
 import { expressPlugin } from '@nammatham/express';
 import simpleTimer from './functions/simple-timer';
 import { app } from './nammatham';
+import testTrigger from './functions/test-trigger';
 
-app.addFunctions(simpleTimer);
+app.addFunctions(simpleTimer, testTrigger);
 
 app.register(
   expressPlugin({
