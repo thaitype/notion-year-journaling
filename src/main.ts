63a33c4c8490ec594dae4b2d0e86e3885ca6d481
Thada Wangthammang <thada.wth@gmail.com>
2024-01-22 11:26:33 +0700

    upgrade nammatham 2.0.0.alpha.12

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import 'dotenv/config';
-import { expressPlugin } from '@nammatham/express';
+import { expressPlugin } from 'nammatham';
 import simpleTimer from './functions/simple-timer';
 import { app } from './nammatham';
 import testTrigger from './functions/test-trigger';
 
 app.addFunctions(simpleTimer, testTrigger);
 
+const dev = process.env.NODE_ENV === 'development';
 app.register(
   expressPlugin({
+    dev,
     allowAllFunctionsAccessByHttp: true,
   })
 );
